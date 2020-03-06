a15d088be192f0012d010b54ea6bc88a3298ddd6
Mateus Cirino <mateuscirinosilva32@gmail.com>
2020-03-06 00:07:24 -0300

    adicao de validacao e adicao de uma classe

--- js/script.js
+++ js/script.js
@@ -3,5 +3,13 @@ trs.forEach(function(tr){
     let imc = tr.querySelector('.info-imc');
     let massa = tr.querySelector('.info-peso');
     let altura = tr.querySelector('.info-altura');
-    imc.textContent = (massa.textContent/(altura.textContent * altura.textContent)).toFixed(2);
+    if(massa.textContent >= 1000 || massa.textContent <= 0) {
+        imc.textContent = 'Peso inválido';
+        tr.classList.add('paciente-invalido')
+    }else if(altura.textContent >= 3.0 || altura.textContent <= 0) {
+        imc.textContent = 'Altura inválida';
+        tr.classList.add('paciente-invalido')
+    }else{
+        imc.textContent = (massa.textContent/(altura.textContent * altura.textContent)).toFixed(2);
+    }
 });
