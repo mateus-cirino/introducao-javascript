fa504134668c89314664827c5a9b1a83fed80d73
Mateus Cirino <mateuscirinosilva32@gmail.com>
2020-03-23 17:11:00 -0300

    calcula-imc somente calcula e retorna o valor

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -1,17 +1,7 @@
-function calculaImc() {
-    let trs = document.querySelectorAll('.paciente');
-    trs.forEach(function(tr){
-        let imc = tr.querySelector('.info-imc');
-        let massa = tr.querySelector('.info-peso');
-        let altura = tr.querySelector('.info-altura');
-        if(massa.textContent >= 1000 || massa.textContent <= 0) {
-            imc.textContent = 'Peso inválido';
-            tr.classList.add('paciente-invalido')
-        }else if(altura.textContent >= 3.0 || altura.textContent <= 0) {
-            imc.textContent = 'Altura inválida';
-            tr.classList.add('paciente-invalido')
-        }else{
-            imc.textContent = (massa.textContent/(altura.textContent * altura.textContent)).toFixed(2);
-        }
-    });
+function calculaImc(peso, altura) {
+    var imc = 0;
+
+    imc = (peso/(altura * altura)).toFixed(2);
+
+    return imc;
 }
